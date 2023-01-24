235145a907528f6959eabaa9e1e083aa3e20c946
Mr.Gamer <nicklaus.vega21@gmail.com>
2023-01-23 21:36:31 -0600

    finsih

--- pyChatbotAIExperiment/training.py
+++ pyChatbotAIExperiment/training.py
@@ -57,3 +57,28 @@ for document in documents:
     output_row = list(output_empty)
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
+
+# Shuffle the features and make numpy array
+random.shuffle(training)
+training = np.array(training)
+
+# Create train and test lists
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+# Create model
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+# Compile model
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+# Fit the model
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.h5', hist)
+print("Done training model")
