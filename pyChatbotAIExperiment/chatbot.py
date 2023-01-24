08378dd1033d387560b4ef296d5b65a181dccc4d
Mr.Gamer <nicklaus.vega21@gmail.com>
2023-01-23 22:37:00 -0600

    done with terminal chat bot base

---
+++ pyChatbotAIExperiment/chatbot.py
@@ -0,0 +1,62 @@
+import random
+import json
+import pickle
+import numpy as np
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+from tensorflow.keras.models import load_model
+
+# Lemmatizer
+lemmatizer = WordNetLemmatizer()
+intends = json.loads(open('intents.json').read())
+
+# Load the model
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = load_model('chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words]
+    return sentence_words
+
+def bow(sentence, words, show_details=False):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for s in sentence_words:
+        for i, word in enumerate(words):
+            if word == s:
+                bag[i] = 1
+                if show_details:
+                    print('found in bag: %s' % word)
+    return np.array(bag)
+
+def predict_class(sentence, model):
+    p = bow(sentence, words, show_details=False)
+    res = model.predict(np.array([p]))[0]
+    ERROR_THRESHOLD = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append({'intent': classes[r[0]], 'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
+
+print('Bot is running!')
+
+while True:
+    message = input('')
+    ints = predict_class(message, model)
+    res = get_response(ints, intends)
+    print(res)
--- pyChatbotAIExperiment/training.py
+++ pyChatbotAIExperiment/training.py
@@ -6,6 +6,8 @@ import numpy as np
 
 import nltk
 nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 from nltk.stem import WordNetLemmatizer
 
 from tensorflow.keras.models import Sequential
